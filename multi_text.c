32a378e1a97925696d56a5c2d1f3e7ee5315b21a
Josh <crumjo@mail.gvsu.edu>
2018-02-07 21:27:57 -0500

    Add files via upload

---
+++ multi_text.c
@@ -0,0 +1,33 @@
+/**
+ * A multi-threaded text search that takes file
+ * names from the command line and prompts the user
+ * to enter text to search for.
+ *
+ * @author Joshua Crum
+ * @version 2/13/2018
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main (int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Pass file name(s) as arguments.\n");
+        return 1;
+    }
+    
+    printf()
+    
+    return 0;
+}
+
+
+int search (void *arg)
+{
+    
+    return 0;
+}
