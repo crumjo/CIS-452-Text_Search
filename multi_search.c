451e1c0d3078a38f89ead7a7687c62566c54dc01
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2018-02-14 14:42:34 -0500

    To do: figure out which child sent what.

--- multi_search.c
+++ multi_search.c
@@ -8,6 +8,8 @@
  */
 
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,6 +20,40 @@
 #define READ_END 0
 #define WRITE_END 1
 
+int num_files;
+static pid_t pid;
+
+/* Array of all child processes. */
+pid_t pros[10];
+
+
+/**
+ * Handles the shutdown of the program and its child processes when
+ * ctrl+c is pressed.
+ *
+ * @param sigNum the signal number for ctrl+c.
+ */
+void sigHandler (int sigNum) {
+    if(pid)
+    {
+        printf (" interrupt received.\n");
+
+        /* Shutdown code. */
+        for (int i = 0; i < num_files; i++)
+        {
+            printf("Shutting down child [%d]...\n", pros[i]);
+            if ((kill (pros[i], SIGKILL)) == -1)
+            {
+                fprintf(stderr, "Child shutdown failed.\n");
+                exit (1);
+            }
+        }
+        
+        sleep (1);
+        printf ("Program will now exit.\n");
+    }
+    exit(0);
+}
 
 
 /**
@@ -25,6 +61,8 @@
  */
 int main (int argc, char **argv)
 {
+    signal(SIGINT, sigHandler);
+    
     if (argc < 2)
     {
         printf ("Pass file name(s) as arguments.\n");
@@ -48,38 +86,31 @@ int main (int argc, char **argv)
     
     int p_to_c[2];
     int c_to_p[2];
-    int num_files = --argc;
+    num_files = --argc;
     
-    /* Get search string from user. */
-    printf("Enter a string to search for: ");
-    char to_search[32];
-    fgets(to_search, 32, stdin);
+    /* Create the pipe from parent to child. */
+    if (pipe(p_to_c) == -1)
+    {
+        fprintf (stderr, "Pipe failed.\n");
+        return 1;
+    }
     
-    /* Remove trailing newline. */
-    int len = strlen(to_search);
-    if (to_search[len - 1] == '\n')
-        to_search[len - 1] = '\0';
+    /* Create the pipe from child to parent. */
+    if (pipe(c_to_p) == -1)
+    {
+        fprintf (stderr, "Pipe failed.\n");
+        return 1;
+    }
     
-    pid_t pid, wpid;
-    pid_t pros[argc];
+    int pid_size;
+    pid_t ppid, wpid;
+    pid_t which_file[argc];
     
     for (int i = 0; i < num_files; i++)
     {
-        /* Create the pipe from parent to child. */
-        if (pipe(p_to_c) == -1)
-        {
-            fprintf (stderr, "Pipe failed.\n");
-            return 1;
-        }
-        
-        /* Create the pipe from child to parent. */
-        if (pipe(c_to_p) == -1)
-        {
-            fprintf (stderr, "Pipe failed.\n");
-            return 1;
-        }
         
         pid = fork();
+        pid_size = sizeof(pid);
         if (pid < 0)
         {
             fprintf (stderr, "Fork failed.\n");
@@ -89,55 +120,128 @@ int main (int argc, char **argv)
         /* Parent. */
         else if (pid)
         {
+            ppid = getpid();
             pros[i] = pid;
-
-            close (p_to_c[READ_END]);
-            close (c_to_p[WRITE_END]);
             
-            write (p_to_c[WRITE_END], to_search, strlen(to_search) + 1);
-            close (p_to_c[WRITE_END]);
-            
-            int p_read_msg;
-            read (c_to_p[READ_END], &p_read_msg, 2);
-            printf ("Receiving data from child...\n");
-
-            close (c_to_p[READ_END]);
-            printf ("Got from child: %d\n", p_read_msg);
-            printf ("The word '%s' occurs in %s %d time(s).\n\n"
-                    , to_search, argv[i+1], p_read_msg);
             continue;
         }
         
         /* Child. */
         else
         {
-            sleep(2);
-            printf ("Creating child process...\n");
-            char c_read_msg[32];
-            close (p_to_c[WRITE_END]);
-            close (c_to_p[READ_END]);
-            
-            read (p_to_c[READ_END], c_read_msg, 32);
-            close (p_to_c[READ_END]);
-            printf("Child of: %d \t My pid: %d \t\t Message: %s\n\n"
-                   , getppid(), getpid(), c_read_msg);
-            
-            int count = search_for (argv[i + 1], c_read_msg);
-            
-            write (c_to_p[WRITE_END], &count, 2);
-            close (c_to_p[WRITE_END]);
+            which_file[i] = getpid();
             break;
         }
-
+        
     }
-
-    while ((wpid = wait(0)) > 0);
     
-//    printf("All child processes: \n");
-//    for(int i = 0; i < 2; i++)
-//    {
-//        printf("%d\n", pros[i]);
-//    }
+    int temp = getpid();
+    
+    /* Close unused pipe ends. */
+    if (temp == ppid)
+    {
+        if ((close (p_to_c[READ_END])) == -1)
+        {
+            fprintf(stderr, "Parent read pipe close failed.\n");
+            return 1;
+        }
+        
+        if ((close (c_to_p[WRITE_END])) == -1)
+        {
+            fprintf(stderr, "Parent write pipe close failed.\n");
+            return 1;
+        }
+    }
+    
+    /* Close child pipe ends. */
+    else
+    {
+        if ((close (p_to_c[WRITE_END])) == -1)
+        {
+            fprintf(stderr, "Child write pipe close failed.\n");
+            return 1;
+        }
+        
+        if ((close (c_to_p[READ_END])) == -1)
+        {
+            fprintf(stderr, "Child read pipe close failed.\n");
+            return 1;
+        }
+    }
+    
+    
+    /* Parent write to pipe. */
+    if (temp == ppid)
+    {
+        while (1)
+        {
+            /* Get search string from user. */
+            printf("\nEnter a string to search for: ");
+            char to_search[1024];
+            fgets(to_search, 1024, stdin);
+            
+            /* Remove trailing newline. */
+            int len = strlen(to_search);
+            if (to_search[len - 1] == '\n')
+                to_search[len - 1] = '\0';
+            
+            int size = sizeof(to_search);
+            
+            for (int i = 0; i < num_files; i++)
+            {
+                if ((write (p_to_c[WRITE_END], &to_search, size)) == -1)
+                {
+                    fprintf (stderr, "Pipe write failed.\n");
+                    return 1;
+                }
+            }
+            
+            for (int j = 0; j < num_files; j++)
+            {
+                int val;
+                if ((read (c_to_p[READ_END], &val, pid_size)) == -1)
+                {
+                    fprintf(stderr, "Read failed.\n");
+                    return 1;
+                }
+                printf ("Count from child: %d\n", val);
+            }
+
+        }
+    }
+    
+    /* Child reads from pipe. */
+    else
+    {
+        while (1)
+        {
+            char *filename = (char *) malloc (32 * sizeof(char));
+            for(int i = 0; i < num_files; i++)
+            {
+                if (which_file[i] == getpid())
+                {
+                    filename = argv[i + 1];
+                }
+            }
+            
+            char c_read[1024];
+            if ((read (p_to_c[READ_END], &c_read, 1024)) == -1)
+            {
+                fprintf(stderr, "Pipe read failed.\n");
+                return 1;
+            }
+            printf ("Child [%d] searching for '%s' in %s\n"
+                    , getpid(), c_read, filename);
+            
+            int count = search_for (filename, c_read);
+            
+            write (c_to_p[WRITE_END], &count, sizeof(count));
+            //free (filename);
+        }
+    }
+    
+    while ((wpid = wait(0)) > 0);
     
     return 0;
 }
+
