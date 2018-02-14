5a26e11b7b8baf02b38c59beb9bdac82b6b49778
Joshua Crum <Josh@Joshuas-MacBook-Air.local>
2018-02-14 14:30:48 -0500

    Temp seems to be working.

--- temp.c
+++ temp.c
@@ -8,6 +8,8 @@
  */
 
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,6 +20,40 @@
 #define READ_END 0
 #define WRITE_END 1
 
+int num_files;
+static pid_t pid;
+
+/* Array of all child processes. */
+pid_t pros[10];
+
+
+/**
+ * Handles the shutdown of the program and its child processes when
+ * ctrl+c is pressed.
+ *
+ * @param sigNum the signal number for ctrl+c.
+ */
+void sigHandler (int sigNum) {
+    if(pid)
+    {
+        printf (" interrupt received.\n");
+
+        /* Shutdown code. */
+        for (int i = 0; i < num_files; i++)
+        {
+            printf("Shutting down child [%d]...\n", pros[i]);
+            if ((kill (pros[i], SIGKILL)) == -1)
+            {
+                fprintf(stderr, "Child shutdown failed.\n");
+                exit (1);
+            }
+        }
+        
+        sleep (1);
+        printf ("Program will now exit.\n");
+    }
+    exit(0);
+}
 
 
 /**
@@ -25,6 +61,8 @@
  */
 int main (int argc, char **argv)
 {
+    signal(SIGINT, sigHandler);
+    
     if (argc < 2)
     {
         printf ("Pass file name(s) as arguments.\n");
@@ -48,17 +86,7 @@ int main (int argc, char **argv)
     
     int p_to_c[2];
     int c_to_p[2];
-    int num_files = --argc;
-    
-    /* Get search string from user. */
-    printf("Enter a string to search for: ");
-    char to_search[1024];
-    fgets(to_search, 1024, stdin);
-    
-    /* Remove trailing newline. */
-    int len = strlen(to_search);
-    if (to_search[len - 1] == '\n')
-        to_search[len - 1] = '\0';
+    num_files = --argc;
     
     /* Create the pipe from parent to child. */
     if (pipe(p_to_c) == -1)
@@ -74,13 +102,15 @@ int main (int argc, char **argv)
         return 1;
     }
     
-    pid_t pid, ppid, wpid;
-    pid_t pros[argc];
+    int pid_size;
+    pid_t ppid, wpid;
+    pid_t which_file[argc];
     
     for (int i = 0; i < num_files; i++)
     {
         
         pid = fork();
+        pid_size = sizeof(pid);
         if (pid < 0)
         {
             fprintf (stderr, "Fork failed.\n");
@@ -93,64 +123,106 @@ int main (int argc, char **argv)
             ppid = getpid();
             pros[i] = pid;
             
-            close (p_to_c[READ_END]);
-            close (c_to_p[WRITE_END]);
-            
             continue;
         }
         
         /* Child. */
         else
         {
-            printf ("Creating child process [%d]...\n", getpid());
-            
-            close (p_to_c[WRITE_END]);
-            close (c_to_p[READ_END]);
-            
+            which_file[i] = getpid();
             break;
         }
         
     }
     
-    int size = sizeof(to_search);
-    printf("Size: %d\n", size);
-    
-    /* Parent write to pipe. */
     int temp = getpid();
     
+    /* Close unused pipe ends. */
     if (temp == ppid)
     {
-        for (int i = 0; i < num_files; i++)
+        close (p_to_c[READ_END]);
+        close (c_to_p[WRITE_END]);
+    }
+    
+    /* Close child pipe ends. */
+    else
+    {
+        close (p_to_c[WRITE_END]);
+        close (c_to_p[READ_END]);
+    }
+    
+    
+    /* Parent write to pipe. */
+    if (temp == ppid)
+    {
+        while (1)
         {
-            printf("Parent writing to pipe with pid %d...\n", ppid);
-            if ((write (p_to_c[WRITE_END], &to_search, size)) == -1)
+            /* Get search string from user. */
+            printf("Enter a string to search for: ");
+            char to_search[1024];
+            fgets(to_search, 1024, stdin);
+            
+            /* Remove trailing newline. */
+            int len = strlen(to_search);
+            if (to_search[len - 1] == '\n')
+                to_search[len - 1] = '\0';
+            
+            int size = sizeof(to_search);
+            
+            for (int i = 0; i < num_files; i++)
             {
-                fprintf (stderr, "Pipe write failed.\n");
-                return 1;
+                if ((write (p_to_c[WRITE_END], &to_search, size)) == -1)
+                {
+                    fprintf (stderr, "Pipe write failed.\n");
+                    return 1;
+                }
             }
+            
+            for (int j = 0; j < num_files; j++)
+            {
+                int val;
+                if ((read (c_to_p[READ_END], &val, pid_size)) == -1)
+                {
+                    fprintf(stderr, "Read failed.\n");
+                    return 1;
+                }
+                printf ("Count from child: %d\n", val);
+            }
+
         }
     }
     
     /* Child reads from pipe. */
     else
     {
-        printf("\nChild %d reading from pipe...\n", getpid());
-        char c_read[size];
-        if ((read (p_to_c[READ_END], &c_read, size)) == -1)
+        while (1)
         {
-            fprintf(stderr, "Pipe read failed.\n");
-            return 1;
+            char *filename = (char *) malloc (32 * sizeof(char));
+            for(int i = 0; i < num_files; i++)
+            {
+                if (which_file[i] == getpid())
+                {
+                    filename = argv[i + 1];
+                }
+            }
+            
+            char c_read[1024];
+            if ((read (p_to_c[READ_END], &c_read, 1024)) == -1)
+            {
+                fprintf(stderr, "Pipe read failed.\n");
+                return 1;
+            }
+            printf ("Child [%d] searching for '%s' in %s\n"
+                    , getpid(), c_read, filename);
+            
+            int count = search_for (filename, c_read);
+            
+            write (c_to_p[WRITE_END], &count, sizeof(count));
+            //free (filename);
         }
-        printf ("[%d] Pipe reading: %s\n", getpid(), c_read);
     }
     
     while ((wpid = wait(0)) > 0);
-    
-    //    printf("All child processes: \n");
-    //    for(int i = 0; i < 2; i++)
-    //    {
-    //        printf("%d\n", pros[i]);
-    //    }
     
     return 0;
 }
